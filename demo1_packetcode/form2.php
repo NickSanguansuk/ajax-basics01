31825d45e44255179fc236aa7c435d938b08a853
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-11-03 23:40:18 -0500

    Added initial files

---
+++ demo1_packetcode/form2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Crash Course</title>
+</head>
+<body>
+
+    <!-- Normal Form -->
+    <form action="submit2.php" method="GET">
+        <input type="text" name="username">
+        <button>Submit2</button>
+    </form>
+
+    <script>
+
+
+        
+    </script>
+
+</body>
+</html>
